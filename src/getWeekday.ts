2195e5b4b14a4df8f5207274d0f1138f493bf2e4
Sanich <sancho@cdrpro.ru>
2021-11-10 14:44:13 +0700

    Updated getWeekday

--- src/getWeekday.ts
+++ src/getWeekday.ts
@@ -1,5 +1,5 @@
 import { DAYS_IN_WEEK } from './constants';
 
-export function getWeekday(d: ReadonlyDate, fromMonday?: boolean): number {
+export function getWeekday(d: ReadonlyDate, fromMonday: boolean = true): number {
   return fromMonday ? (d.getDay() + 6) % DAYS_IN_WEEK : d.getDay();
 }
